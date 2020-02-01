e552a9320cd8a9e3e5fd64a956a2eb76ed6f957b
Sameera Palliyage <sameeralaknath@gmail.com>
2020-02-01 10:51:48 +0530

    'init'

---
+++ react-app/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { dCandidate } from "./dCandidate";
+
+// combine reducers if there are more
+export const reducers = combineReducers({
+    dCandidate
+})
